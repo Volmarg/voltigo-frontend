5a969bf211fbfe01438dee425a40ccaea31d3a24
Volmarg <dwlodarczyk12@gmail.com>
2024-07-27 07:50:58 +0200

    :bug: Add missing dev views

---
+++ src/views/Development/Playground.vue
@@ -0,0 +1,16 @@
+<template>
+  <!-- Keep this page clean, don't let users reach the code that could bypass something here !-->
+  <div></div>
+</template>
+
+<script lang="ts">
+
+import {ComponentData} from "@/scripts/Vue/Types/Components/types";
+
+export default {
+  data(): ComponentData {
+    return {}
+  },
+
+}
+</script>
