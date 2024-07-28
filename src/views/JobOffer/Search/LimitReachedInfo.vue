a5247060abbe765be7a06899a4afa2d2107626b0
Volmarg <dwlodarczyk12@gmail.com>
2024-07-28 13:17:23 +0200

    :clown_face: Add demo info to Search > LimitReachedInfo

--- src/views/JobOffer/Search/LimitReachedInfo.vue
+++ src/views/JobOffer/Search/LimitReachedInfo.vue
@@ -1,14 +1,29 @@
 <template>
   <div>
 
+    <WarningBlock class="bg-white" v-if="isDemo">
+      <div class="text flex justify-center flex-col">
+        <p class="font-bold text-orange-900 text-xl">
+          <span>{{$t('demo.sectionInfo.header')}}</span>
+        </p>
+
+        <p class="font-bold text-orange-900 text-2md mt-3">
+          <span>{{$t('demo.sectionInfo.info')}}</span>
+        </p>
+      </div>
+    </WarningBlock>
+
     <WarningBlock :text="$t('jobOfferSearch.page.limitReached')"
                   class="mb-5 bg-white text-gray-500 text-2md"
+                  v-else
     />
 
   </div>
 </template>
 
 <script lang="ts">
+import DemoAwareMixin from "@/mixins/Awarness/DemoAwareMixin.vue";
+
 import WarningBlock    from "@/components/Page/WarningBlock.vue";
 import {ComponentData} from "@/scripts/Vue/Types/Components/types";
 import UserController  from "@/scripts/Core/Controller/UserController";
@@ -21,6 +36,9 @@ export default {
   },
   components: {
     WarningBlock,
-  }
+  },
+  mixins: [
+    DemoAwareMixin
+  ]
 }
 </script>
